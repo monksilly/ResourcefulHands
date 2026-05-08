6635561084e6f6a820de8c4e527581722d568146
galfar <60539808+galfar-coder@users.noreply.github.com>
2026-05-08 23:33:15 +0200

    feat: Kinda proper PackManager

--- Assets/PackManager.cs
+++ Assets/PackManager.cs
@@ -1,19 +1,47 @@
 using System.Collections.Generic;
+using ResourcefulHands.Core;
+using ResourcefulHands.Utility;
+using WKLib.API;
 
 namespace ResourcefulHands.Assets;
 
-public class PackManager
+public static class PackManager
 {
-    public List<Cosmetic_HandItem> handCosmetics = [];
-    public List<Cosmetic_Voice> voiceCosmetics = [];
+    public static List<CosmeticHandPack> ActiveHandPacks { get; private set; }
 
-    public void GatherHands()
+    public static List<CosmeticHandPack> HandPacks
     {
-        handCosmetics = CL_CosmeticManager.cosmeticHands;
+        get => _handCosmetics; 
     }
 
-    public void GatherVoices()
+    public static List<ICosmeticPack> ActiveCosmeticPacks { get; private set; } = [];
+    public static List<ICosmeticPack> CosmeticPacks { get; private set; } = [];
+    
+    private static List<CosmeticHandPack> _handCosmetics = [];
+    private static List<Cosmetic_Voice> _voiceCosmetics = [];
+    
+    public static bool RegisterHandCosmetic(CosmeticHandPack handPack)
     {
-        voiceCosmetics = CL_CosmeticManager.cosmeticVoices;
+        if (_handCosmetics.Contains(handPack)) return false;
+        
+        ModLogger.Info($"Pack {handPack.id} has been registered");
+        _handCosmetics.Add(handPack);
+        CosmeticPacks.Add(handPack);
+        return true;
+    }
+
+    public static void GatherGameHandCosmetics()
+    {
+        foreach (var cosmeticHandPack in CL_CosmeticManager.cosmeticHands)
+            _handCosmetics.Add((cosmeticHandPack.cosmeticData as CosmeticHandPack)!);
+    }
+    
+    public static bool IncludesPack(CosmeticHandPack handPack) => _handCosmetics.Contains(handPack);
+
+    public static List<CosmeticHandPack> GetHandPacks() => _handCosmetics;
+
+    public static void ShowPopup(string text, float timeToShow)
+    {
+        WKLib.API.UI.UIUtility.ShowPopupForTime(text, timeToShow);
     }
 }
