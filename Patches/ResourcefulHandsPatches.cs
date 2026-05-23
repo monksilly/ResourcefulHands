50b06f4aaaae7a7a19771fb1f9e7ea9d60fed4c0
ikeiwa <evann.delaunay@gmail.com>
2026-05-23 19:43:53 +0200

    refactor: All custom hand animation/sprite change is now done in a custom monobehavior on the hand

--- Patches/ResourcefulHandsPatches.cs
+++ Patches/ResourcefulHandsPatches.cs
@@ -19,117 +19,6 @@ namespace ResourcefulHands.Patches;
 [HarmonyPatch]
 public class ResourcefulHandsPatches
 {
-    private class HandState
-    {
-        public ENT_Player.Hand Hand;
-        public EmoteEntry? CurrentEmote;
-
-        public int EmoteSoundIndex;
-        public AudioSource? EmoteAudioSource;
-        public float EmoteTime;
-
-        public Vector3 OriginalOffset;
-        public Vector3 BaseScaleFactor;
-        public Vector3 OriginalScale;
-        public Quaternion OriginalRotation;
-
-        public void SetEmote(EmoteEntry? emote, bool force = false)
-        {
-            if (CurrentEmote != null && !force || emote == null) return;
-            bool changedEmote = CurrentEmote != emote;
-            
-            CurrentEmote = emote; 
-            if (changedEmote)
-                EmoteAudioSource = AudioUtility.PlaySound(
-                    GetEmoteSound(), Hand.handModel.position, Hand.handModel,
-                    loop: CurrentEmote.SoundLoop, bypassEffects: true, mixerType: AudioMixerType.Sfx);
-            
-            if(CurrentEmote.PlayMode is EmotePlayMode.Loop or EmotePlayMode.Once)
-                EmoteTime = Time.time;
-        }
-
-        public Vector3 GetOffset(float side)
-        {
-            if (CurrentEmote != null)
-                return new Vector3(CurrentEmote.position.x * -side, CurrentEmote.position.y, CurrentEmote.position.z);
-
-            return OriginalOffset;
-        }
-
-        public Vector3 GetScale(Vector3 currentScale)
-        {
-            if (CurrentEmote != null)
-                return Vector3.Lerp(currentScale, Vector3.Scale(CurrentEmote.Scale, BaseScaleFactor), Time.deltaTime * 6f);
-
-            return OriginalScale;
-        }
-
-        public Quaternion GetRotation(float side)
-        {
-            if (CurrentEmote != null)
-                return Quaternion.Euler(0, 0, CurrentEmote.Rotation * side);
-
-            return OriginalRotation;
-        }
-
-        public AudioClip? GetEmoteSound()
-        {
-            if (EmoteAudioSource && EmoteAudioSource.isPlaying) EmoteAudioSource.Stop();
-            if (CurrentEmote == null) return null;
-            if (CurrentEmote.SoundClips == null || CurrentEmote.SoundClips.Count == 0) return null;
-
-            switch (CurrentEmote.SoundPlayMode)
-            {
-                case SoundPlayMode.Random:
-                    return CurrentEmote.SoundClips[Random.Range(0, CurrentEmote.SoundClips.Count)];
-                case SoundPlayMode.Sequential:
-                    var clip = CurrentEmote.SoundClips[EmoteSoundIndex];
-                    EmoteSoundIndex = (EmoteSoundIndex + 1) % CurrentEmote.SoundClips.Count;
-                    return clip;
-            }
-
-            return null;
-        }
-
-        public void StopEmote()
-        {
-            if (CurrentEmote == null) return;
-
-            ModLogger.Debug("Stopped Emote " + CurrentEmote.name);
-            if (CurrentEmote.SoundLoop)
-                EmoteAudioSource?.Stop();
-            EmoteAudioSource = null;
-            CurrentEmote = null;
-        }
-
-        public void ApplyState()
-        {
-            ApplyEmote();
-        }
-
-        private void ApplyEmote()
-        {
-            if (CurrentEmote == null) return;
-            var spriteIndex = 0;
-
-            switch (CurrentEmote.PlayMode)
-            {
-                case EmotePlayMode.Loop:
-                    spriteIndex = Mathf.FloorToInt(Mathf.Repeat((Time.time-EmoteTime) * CurrentEmote.Framerate, CurrentEmote.Sprites.Count));
-                    break;
-                case EmotePlayMode.LoopGlobal:
-                    spriteIndex = Mathf.FloorToInt(Mathf.Repeat(Time.time * CurrentEmote.Framerate, CurrentEmote.Sprites.Count));
-                    break;
-                case EmotePlayMode.Once:
-                    spriteIndex = Mathf.FloorToInt(Mathf.Min((Time.time-EmoteTime) * CurrentEmote.Framerate, CurrentEmote.Sprites.Count-1));
-                    break;
-            }
-            
-            Hand.SetSprite(CurrentEmote.Sprites[spriteIndex]);
-        }
-    }
-
-    private static HandState[]? _handStates;
 
     [HarmonyPatch(typeof(CL_CosmeticManager))]
     public static class CL_CosmeticManager_Patches
@@ -244,70 +133,12 @@ public class ResourcefulHandsPatches
     public static class ENT_Player_Patches
     {
         [HarmonyPostfix]
-        [HarmonyPatch("Awake")]
-        static void AwakePostfix(ENT_Player __instance)
-        {
-            _handStates = new HandState[__instance.hands.Length];
-            for (int i = 0; i < _handStates.Length; i++)
-            {
-                _handStates[i] = new HandState();
-                _handStates[i].Hand = __instance.hands[i];
-            }
-        }
-
-        [HarmonyPostfix]
         [HarmonyPatch("HandAnimation")]
         public static void HandAnimationPostfix(ENT_Player __instance, ENT_Player.Hand curhand, bool interacting,
             bool canInteract)
         {
-            if (_handStates == null) return;
-            
-            ApplyEmotes(curhand, interacting, canInteract);
-            _handStates[curhand.id].ApplyState();
-        }
-
-        private static void ApplyEmotes(ENT_Player.Hand hand, bool interacting, bool canInteract)
-        {
-            if (hand.currentCosmetics == null || hand.currentCosmetics.Count == 0)
-                return;
-
-            if (interacting || !canInteract || !hand.IsFree())
-            {
-                _handStates[hand.id].StopEmote();
-                return;
-            }
-
-            bool isLeft = hand.id == 0;
-            var keyBinds = isLeft ? RHConfig.EmoteKeysLeft : RHConfig.EmoteKeysRight;
-
-            foreach (var cosmetic in hand.currentCosmetics)
-            {
-                if (!PackManager.HandCosmeticPacksDict.TryGetValue(cosmetic.cosmeticData.id, out var pack))
-                    continue;
-
-                if (pack.ExtendedCosmeticData.emotes == null || pack.ExtendedCosmeticData.emotes.Count == 0)
-                    continue;
-
-                bool playingEmote = false;
-                for (int i = 0; i < Mathf.Min(pack.ExtendedCosmeticData.emotes.Count, RHConfig.MaxEmotes); i++)
-                {
-                    if (keyBinds[i].Value == KeyCode.None) continue;
-                    if (!InputUtility.GetKey(keyBinds[i].Value)) continue;
-
-                    var emote = pack.ExtendedCosmeticData.emotes[i];
-
-                    if (emote.Sprites.Count == 0) continue;
-                    var handState = _handStates[hand.id];
-                    handState.SetEmote(emote);
-
-                    playingEmote = true;
-                    break;
-                }
-
-                if (playingEmote) continue;
-
-                _handStates[hand.id].StopEmote();
-            }
+            if(HandExtensions.TryGet(curhand, out var handExtension))
+                handExtension.ApplySprite();
         }
     }
 
@@ -318,51 +149,49 @@ public class ResourcefulHandsPatches
         [HarmonyPatch(nameof(ViewSway.Start))]
         static void StartPostfix(ViewSway __instance)
         {
-            if (_handStates == null) return;
-            _handStates[__instance.hand.id].OriginalScale = __instance.hand.handModel.localScale;
-            _handStates[__instance.hand.id].BaseScaleFactor = __instance.hand.handModel.localScale;
+            if(!HandExtensions.TryGet(__instance.hand, out var handExtension)) return;
+            handExtension.originalScale = __instance.hand.handModel.localScale;
+            handExtension.baseScaleFactor = __instance.hand.handModel.localScale;
         }
 
         [HarmonyPrefix]
         [HarmonyPatch(nameof(ViewSway.Update))]
         static void UpdatePrefix(ViewSway __instance)
         {
-            if (_handStates == null) return;
-            var handState = _handStates[__instance.hand.id];
-            handState.OriginalOffset = __instance.targetOffset;
-            handState.OriginalRotation = __instance.swayRot;
+            if(!HandExtensions.TryGet(__instance.hand, out var handExtension)) return;
+            handExtension.originalOffset = __instance.targetOffset;
+            handExtension.originalRotation = __instance.swayRot;
 
-            float handSide = __instance.hand.id == 0 ? -1f : 1f;
-            __instance.targetOffset = handState.GetOffset(handSide);
+            handExtension.ApplyOffset();
         }
 
         [HarmonyPostfix]
         [HarmonyPatch(nameof(ViewSway.Update))]
         static void UpdatePostfix(ViewSway __instance)
         {
-            if (_handStates == null) return;
-            var handState = _handStates[__instance.hand.id];
-            float handSide = __instance.hand.id == 0 ? -1f : 1f;
+            if(!HandExtensions.TryGet(__instance.hand, out var handExtension)) return;
             
-            __instance.targetOffset = handState.OriginalOffset;
-            __instance.transform.localRotation = Quaternion.Lerp(__instance.transform.localRotation,
-                handState.GetRotation(handSide) *
-                Quaternion.Euler(Vector3.ClampMagnitude(Random.insideUnitSphere * __instance.shakeAmount * 30f,
-                    20.5f)) * Quaternion.Euler(0.0f, 0.0f,
-                    Mathf.Sin(__instance.rockAmount + Time.time) + __instance.parameters.bobBaseRotation * handSide),
-                Time.deltaTime * 6f);
+            __instance.targetOffset = handExtension.originalOffset;
+            handExtension.ApplyRotation();
         }
     }
     
     [HarmonyPatch(typeof(ENT_Player.Hand))]
     public static class Hand_Patches
     {
+        [HarmonyPostfix]
+        [HarmonyPatch(nameof(ENT_Player.Hand.Initialize))]
+        static void InitializePost(ENT_Player.Hand __instance)
+        {
+            __instance.handBase.gameObject.AddComponent<HandExtensions>();
+        }
+        
         [HarmonyPrefix]
         [HarmonyPatch(nameof(ENT_Player.Hand.SetScale))]
         static bool UpdatePrefix(ENT_Player.Hand __instance, Vector3 scale)
         {
-            if (_handStates == null) return true;
-            _handStates[__instance.id].OriginalScale = scale;
+            if(!HandExtensions.TryGet(__instance, out var handExtension)) return true;
+            handExtension.originalScale = scale;
             return false;
         }
         
@@ -370,9 +199,8 @@ public class ResourcefulHandsPatches
         [HarmonyPatch(nameof(ENT_Player.Hand.ScaleUpdate))]
         static void LateUpdatePost(ENT_Player.Hand __instance)
         {
-            if (_handStates == null) return;
-            var handState = _handStates[__instance.id];
-            __instance.handModel.localScale = handState.GetScale(__instance.handModel.localScale);
+            if(!HandExtensions.TryGet(__instance, out var handExtension)) return;
+            handExtension.ApplyScale();
         }
     }
 }
---
+++ Systems/HandExtensions.cs
@@ -0,0 +1,199 @@
+using System;
+using System.Collections.Generic;
+using ResourcefulHands.Assets;
+using ResourcefulHands.Core;
+using ResourcefulHands.Utility;
+using UnityEngine;
+using UnityEngine.Serialization;
+using WKLib.API.Audio;
+using WKLib.API.Input;
+using WKLib.Core.Classes;
+using Random = UnityEngine.Random;
+
+namespace ResourcefulHands.Systems;
+
+public class HandExtensions : MonoBehaviour
+{
+    private static readonly Dictionary<ENT_Player.Hand, HandExtensions> _handExtensionsMap = new();
+
+    private ENT_Player.Hand _hand;
+    private EmoteEntry? _currentEmote;
+
+    private int _emoteSoundIndex;
+    private AudioSource? _emoteAudioSource;
+    private float _emoteTime;
+
+    public Vector3 originalOffset;
+    public Vector3 baseScaleFactor;
+    public Vector3 originalScale;
+    public Quaternion originalRotation;
+
+    private bool IsLeft => _hand.id == 0;
+
+    private void Awake()
+    {
+        _hand = GetComponent<Hand_Base>().hand;
+        _handExtensionsMap.Add(_hand, this);
+    }
+
+    private void OnDestroy()
+    {
+        _handExtensionsMap.Remove(_hand);
+    }
+
+    private void Update()
+    {
+        if (_hand.currentCosmetics == null || _hand.currentCosmetics.Count == 0)
+            return;
+
+        UpdateEmotes();
+    }
+
+    private void UpdateEmotes()
+    {
+        if (!_hand.IsFree())
+        {
+            StopEmote();
+            return;
+        }
+
+        var keyBinds = IsLeft ? RHConfig.EmoteKeysLeft : RHConfig.EmoteKeysRight;
+
+        foreach (var cosmetic in _hand.currentCosmetics)
+        {
+            if (!PackManager.HandCosmeticPacksDict.TryGetValue(cosmetic.cosmeticData.id, out var pack))
+                continue;
+
+            if (pack.ExtendedCosmeticData.emotes == null || pack.ExtendedCosmeticData.emotes.Count == 0)
+                continue;
+
+            bool playingEmote = false;
+            for (int i = 0; i < Mathf.Min(pack.ExtendedCosmeticData.emotes.Count, RHConfig.MaxEmotes); i++)
+            {
+                if (keyBinds[i].Value == KeyCode.None) continue;
+                if (!InputUtility.GetKey(keyBinds[i].Value)) continue;
+
+                var emote = pack.ExtendedCosmeticData.emotes[i];
+
+                if (emote.Sprites.Count == 0) continue;
+                SetEmote(emote);
+
+                playingEmote = true;
+                break;
+            }
+
+            if (playingEmote) continue;
+
+            StopEmote();
+        }
+    }
+
+    public static bool TryGet(ENT_Player.Hand? hand, out HandExtensions? ext)
+    {
+        ext = null;
+        return hand != null && _handExtensionsMap.TryGetValue(hand, out ext);
+    }
+
+    public void ApplySprite()
+    {
+        ApplyEmote();
+    }
+    
+    private void ApplyEmote()
+    {
+        if (_currentEmote == null) return;
+        var spriteIndex = 0;
+
+        switch (_currentEmote.PlayMode)
+        {
+            case EmotePlayMode.Loop:
+                spriteIndex = Mathf.FloorToInt(Mathf.Repeat((Time.time-_emoteTime) * _currentEmote.Framerate, _currentEmote.Sprites.Count));
+                break;
+            case EmotePlayMode.LoopGlobal:
+                spriteIndex = Mathf.FloorToInt(Mathf.Repeat(Time.time * _currentEmote.Framerate, _currentEmote.Sprites.Count));
+                break;
+            case EmotePlayMode.Once:
+                spriteIndex = Mathf.FloorToInt(Mathf.Min((Time.time-_emoteTime) * _currentEmote.Framerate, _currentEmote.Sprites.Count-1));
+                break;
+        }
+            
+        _hand.SetSprite(_currentEmote.Sprites[spriteIndex]);
+    }
+
+    public void SetEmote(EmoteEntry? emote, bool force = false)
+    {
+        if (_currentEmote != null && !force || emote == null) return;
+        bool changedEmote = _currentEmote != emote;
+
+        _currentEmote = emote;
+        if (changedEmote)
+            _emoteAudioSource = AudioUtility.PlaySound(
+                GetEmoteSound(), _hand.handModel.position, _hand.handModel,
+                loop: _currentEmote.SoundLoop, bypassEffects: true, mixerType: AudioMixerType.Sfx);
+
+        if (_currentEmote.PlayMode is EmotePlayMode.Loop or EmotePlayMode.Once)
+            _emoteTime = Time.time;
+    }
+
+    public void ApplyOffset()
+    {
+        if (_currentEmote == null) return;
+        
+        float side = IsLeft ? -1f : 1f;
+        _hand.handSway.targetOffset = new Vector3(_currentEmote.position.x * -side, _currentEmote.position.y, _currentEmote.position.z);
+    }
+
+    public void ApplyScale()
+    {
+        if (_currentEmote == null)
+        {
+            _hand.handModel.localScale = originalScale;
+            return;
+        }
+        
+        _hand.handModel.localScale = Vector3.Lerp(_hand.handModel.localScale, Vector3.Scale(_currentEmote.Scale, baseScaleFactor), Time.deltaTime * 6f);
+    }
+
+    public void ApplyRotation()
+    {
+        if (_currentEmote == null) return;
+        
+        float side = IsLeft ? -1f : 1f;
+        var rotation = Quaternion.Euler(0, 0, _currentEmote.Rotation * side);
+        _hand.handSway.transform.localRotation = Quaternion.Lerp(_hand.handSway.transform.localRotation,
+            rotation *
+            Quaternion.Euler(Vector3.ClampMagnitude(Random.insideUnitSphere * _hand.handSway.shakeAmount * 30f,
+                20.5f)) * Quaternion.Euler(0.0f, 0.0f,
+                Mathf.Sin(_hand.handSway.rockAmount + Time.time) + _hand.handSway.parameters.bobBaseRotation * side),
+            Time.deltaTime * 6f);
+    }
+
+    public AudioClip? GetEmoteSound()
+    {
+        if (_emoteAudioSource && _emoteAudioSource.isPlaying) _emoteAudioSource.Stop();
+        if (_currentEmote?.SoundClips == null || _currentEmote.SoundClips.Count == 0) return null;
+
+        switch (_currentEmote.SoundPlayMode)
+        {
+            case SoundPlayMode.Random:
+                return _currentEmote.SoundClips[Random.Range(0, _currentEmote.SoundClips.Count)];
+            case SoundPlayMode.Sequential:
+                var clip = _currentEmote.SoundClips[_emoteSoundIndex];
+                _emoteSoundIndex = (_emoteSoundIndex + 1) % _currentEmote.SoundClips.Count;
+                return clip;
+        }
+
+        return null;
+    }
+
+    public void StopEmote()
+    {
+        if (_currentEmote == null) return;
+
+        ModLogger.Debug("Stopped Emote " + _currentEmote.name);
+        if (_currentEmote.SoundLoop)
+            _emoteAudioSource?.Stop();
+        _emoteAudioSource = null;
+        _currentEmote = null;
+    }
+}
